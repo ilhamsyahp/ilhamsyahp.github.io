57b9384ec2dec5615443cba56aa91f41c082dd0c
RESTYONO ILHAM SYAHPUTRA <90881397+ilhamsyahp@users.noreply.github.com>
2022-01-03 22:08:36 +0700

    Add files via upload

---
+++ web_portal_berita/page/gallery.php
@@ -0,0 +1,71 @@
+<!-- ================================== bsgian post FASILITAS ================================================ -->
+	<div class="container mt-5">
+		<div class="mx-auto kategori-berita-judul" style="width: 400px;">
+			<h6 class="font-weight-bold text-center " style="font-size: 17px;">FASILITAS</h6 >
+		</div>
+		<hr class="hr-judul" style="width: 210px;">
+		<div class="row">
+			
+			<div class="col-md-4 mt-4 col-sm-2" data-aos="zoom-in">
+				<div class="card" style="width: 18rem;">
+				  <img src="vendor/img/ls.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h5 class="card-title text-center " style="font-size: 16px;">Lahan Sekolah Luas</h5>
+				  </div>
+				</div>
+			</div>
+
+			
+			<div class="col-md-4 mt-4 col-sm-2" data-aos="zoom-in">
+				<div class="card" style="width: 18rem;">
+				  <img src="vendor/img/gs.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h5 class="card-title text-center " style="font-size: 16px;">Gedung Serbaguna</h5>
+				  </div>
+				</div>
+			</div>
+
+			
+			<div class="col-md-4 mt-4 col-sm-2" data-aos="zoom-in">
+				<div class="card" style="width: 18rem;">
+				  <img src="vendor/img/klls.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h5 class="card-title text-center " style="font-size: 16px;">Kelas yang Nyaman</h5>
+				  </div>
+				</div>
+			</div>
+
+			
+			<div class="col-md-4 mt-4 col-sm-2" data-aos="zoom-in">
+				<div class="card" style="width: 18rem;">
+				  <img src="vendor/img/ac.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h5 class="card-title text-center " style="font-size: 16px;">Ruangan AC</h5>
+				  </div>
+				</div>
+			</div>
+
+			
+			<div class="col-md-4 mt-4 col-sm-2" data-aos="zoom-in">
+				<div class="card" style="width: 18rem;">
+				  <img src="vendor/img/wifi.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h5 class="card-title text-center " style="font-size: 16px;">Wifi Area</h5>
+				  </div>
+				</div>
+			</div>
+
+		
+			<div class="col-md-4 mt-4 col-sm-2" data-aos="zoom-in">
+				<div class="card" style="width: 18rem;">
+				  <img src="vendor/img/taman1.jpg" class="card-img-top" alt="...">
+				  <div class="card-body">
+				    <h5 class="card-title text-center " style="font-size: 16px;">Tempat Bermain Anak</h5>
+				  </div>
+				</div>
+			</div>
+
+			<!-- Jika ingin di tambah tinggal copas bagian atas dan lanjutkan -->
+			<br><br><br><br><br><br><br><br><br><br><br><br><br>
+		</div>
+	</div>	
---
+++ web_portal_berita/page/pendaftaran.php
@@ -0,0 +1,12 @@
+   
+   <!-- ================================== bagian detail jurusan rpl ======================================================= -->
+    <div class="mx-auto judul-jurusan" style="width: 400px;">
+      <h6 class="font-weight-bold text-center">PENDAFTARAN PESERTA DIDIK</h6>
+    </div>
+    <hr class="hr-judul-jurusan">
+    <div class="card mt-5 mx-auto" style="width: 18rem;" data-aos="zoom-in">
+      <img src="vendor/img/maaf.jpg" class="card-img-top" alt="...">
+    </div>
+    <div class="container" data-aos="zoom-in">
+      <p class="text-center mt-3" style="font-size: 15px;">Maaf Pendaftaran belum dapat dilakukan Silahkan menunggu Pengumuman pada Halaham Beranda.</p>
+    </div><br>
